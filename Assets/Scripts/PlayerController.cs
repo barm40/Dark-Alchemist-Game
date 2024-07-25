703607e94f2051b63a34af6cbd6d4bcebd03d19e
Alex Ten <alexwww9@gmail.com>
2024-07-25 07:41:22 +0300

    Added follow cam and some particles
    Added camera to follow player
    modified player prefab
    added player idle and walk particles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Stats),typeof(PlayerItemInteractableManager))]
 public class PlayerController : MonoBehaviour
